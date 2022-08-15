9d0b994157bbf491d999a6e2b4310707f6102cf0
Chief-spartan-117 <shresthasagar861@gmail.com>
2022-08-15 17:20:17 +0545

    index.html design

--- script/index.js
+++ script/index.js
@@ -1,30 +1,49 @@
-var typeText = document.querySelector(".type")
-var textToBeTypedArr = ["Faster than Gmail!", "Anonymous (+_+)", "Faster than Yahoo!"]
-var index = 0, isAdding = true, textToBeTypedIndex = 0
+var typeText = document.querySelector(".type");
+var textToBeTypedArr = [
+  "Faster than Gmail! ",
+  "Anonymous (+_+) ",
+  "Faster than Yahoo! ",
+];
+var index = 0,
+  isAdding = true,
+  textToBeTypedIndex = 0;
 
 function playAnim() {
-  setTimeout(function () {
-    typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index)
-    if (isAdding) {
-      if (index > textToBeTypedArr[textToBeTypedIndex].length) {
-        isAdding = false
-        setTimeout(function () {
-          playAnim()
-        }, 2000)
-        return
+  setTimeout(
+    function () {
+      typeText.innerText = textToBeTypedArr[textToBeTypedIndex].slice(0, index);
+      if (isAdding) {
+        if (index > textToBeTypedArr[textToBeTypedIndex].length) {
+          isAdding = false;
+          setTimeout(function () {
+            playAnim();
+          }, 2000);
+          return;
+        } else {
+          index++;
+        }
       } else {
-        index++
+        if (index === 0) {
+          isAdding = true;
+          textToBeTypedIndex =
+            (textToBeTypedIndex + 1) % textToBeTypedArr.length;
+        } else {
+          index--;
+        }
       }
-    } else {
-      if (index === 0) {
-        isAdding = true
-        textToBeTypedIndex = (textToBeTypedIndex + 1) % textToBeTypedArr.length
-      } else {
-        index--
-      }
-    }
-    playAnim()
-  }, isAdding ? 120 : 60)
+      playAnim();
+    },
+    isAdding ? 120 : 60
+  );
 }
 
-playAnim()
+playAnim();
+
+///////////////////////////////////////////////////////////////////
+// Copyright
+const copyright = document.querySelector(".copyright");
+
+const date = new Date();
+console.log(date.getFullYear());
+
+copyright.innerHTML = `[  &copy; Copyright, solo mail service ${date.getFullYear()} ]`;
